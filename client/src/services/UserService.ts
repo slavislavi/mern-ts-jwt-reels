1d3e9c6f8e0166088b6a136c957e930f5bc12487
slavislavi <slavislavi1986@gmail.com>
2022-12-23 15:43:05 +0300

    [main]: auth-service

---
+++ client/src/services/UserService.ts
@@ -0,0 +1,7 @@
+import { AxiosResponse } from "axios";
+import api from "../http";
+import { AuthResponse } from "../models/response/AuthResponse";
+
+export default class UserService {
+  static fetchUsers() {}
+}
